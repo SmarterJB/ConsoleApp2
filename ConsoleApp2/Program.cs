e6495c5ebe81141f8bba9cd70f027cbaf52b90aa
Josef Benda <j.benda@sharxx.com>
2018-07-04 19:19:57 +0200

    Projektdateien hinzufügen.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+
+        /// <summary>
+        /// This is the main method.
+        /// </summary>
+        static void Main()
+        {
+            var word = "hallo";
+            var test = word.LastIndexOf('.');
+        }
+    }
+}
