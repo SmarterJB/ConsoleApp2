8be4c590d954cee565c7ae240bbd7b01a1457092
Josef Benda <j.benda@sharxx.com>
2018-07-04 19:23:02 +0200

    Macht nix

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -17,7 +17,7 @@ namespace ConsoleApp2
         {
             var word = "hallo";
             var test = word.LastIndexOf('.');
-            var result = test.CompareTo(2);
+            var result = test.CompareTo(-1);
         }
     }
 }
